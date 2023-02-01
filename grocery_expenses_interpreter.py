02ed6250e13fefa908bf025151e2779674782aaa
Krishna Narwade <63601581+KrishnaNarwade@users.noreply.github.com>
2023-02-01 12:33:47 +0100

    Adding interpreter for DSL file

---
+++ grocery_expenses_interpreter.py
@@ -0,0 +1,28 @@
+class GroceryExpenseInterpreter:
+    def __init__(self, file_path):
+        self.data = []
+        self._parse(file_path)
+
+    def _parse(self, file_path):
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+            lines = [x.strip() for x in lines]
+
+            for line in lines:
+                if 'item:' in line:
+                    item = line.split(':')[1].strip()
+                elif 'price:' in line:
+                    price = float(line.split(':')[1].strip())
+                    self.data.append({'item': item, 'price': price})
+
+    def total(self):
+        return sum([x['price'] for x in self.data])
+    def item(self):
+        return [x['item'] for x in self.data]
+    def ind_cost(self):
+        return [x['price'] for x in self.data]
+
+expense = GroceryExpenseInterpreter('grocery_expenses.dsl')
+print(f"You have Spent: € {round(expense.total())} in total\nYou have purchased these following items: {expense.item()}\nYour highest purchase amount is {max(expense.ind_cost())}")
+
+#print(f"Expenses details: \n You have purchased: {interpreter.Expense.ItemName} {interpreter.Expense.ItemName}\nprice_Per_unit(In Euros): {interpreter.Expense.ItemPrice}\nItemQuantity: {interpreter.Expense.ItemQuantity}\nPurchaseDate: {interpreter.Expense.PurchaseDate}\nFinalAmount: {interpreter.Expense.FinalAmount}")
