6880ebc190242fd42a2fe6ebba6df00026ce7bee
Krishna Narwade <63601581+KrishnaNarwade@users.noreply.github.com>
2023-02-11 15:18:08 +0100

    Updating main.db

--- mydb.py
+++ mydb.py
@@ -1,3 +1,4 @@
+#1 module
 import sqlite3
 
 #2 class, constructor and methods 
@@ -9,21 +10,21 @@ class Database:
             "CREATE TABLE IF NOT EXISTS expense_record (item_name text, item_price float, purchase_date date)")
         self.conn.commit()
 
-    def Fetch_record(self, query):
+    def fetchRecord(self, query):
         self.cur.execute(query)
         rows = self.cur.fetchall()
         return rows
 
-    def Insert_record(self, item_name, item_price, purchase_date):
+    def insertRecord(self, item_name, item_price, purchase_date):
         self.cur.execute("INSERT INTO expense_record VALUES (?, ?, ?)",
                          (item_name, item_price, purchase_date))
         self.conn.commit()
 
-    def Remove_record(self, rwid):
+    def removeRecord(self, rwid):
         self.cur.execute("DELETE FROM expense_record WHERE rowid=?", (rwid,))
         self.conn.commit()
 
-    def Update_record(self, item_name, item_price, purchase_date, rid):
+    def updateRecord(self, item_name, item_price, purchase_date, rid):
         self.cur.execute("UPDATE expense_record SET item_name = ?, item_price = ?, purchase_date = ? WHERE rowid = ?",
                          (item_name, item_price, purchase_date, rid))
         self.conn.commit()
